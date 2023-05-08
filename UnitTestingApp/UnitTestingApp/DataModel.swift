68c30d143ade9f89f9c56a0c73cf65cc29d4b088
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-08 22:37:58 +0300

    Add struct DataModel

---
+++ UnitTestingApp/UnitTestingApp/DataModel.swift
@@ -0,0 +1,22 @@
+//
+//  DataModel.swift
+//  UnitTestingApp
+//
+//  Created by Арсентий Халимовский on 08.05.2023.
+//
+
+import Foundation
+
+struct DataModel {
+    
+    // MARK: - Public Properties
+    
+    var volume = 0
+    
+    // MARK: - Public Methods
+    
+    func setVolume(to value: Int) {
+        
+    }
+    
+}
