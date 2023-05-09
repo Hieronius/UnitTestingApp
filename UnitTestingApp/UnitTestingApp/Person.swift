2a1135a0ded5fe481bd3bc618b9d8882c57840d8
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-09 15:27:02 +0300

    Add tests for all class properties

--- UnitTestingApp/UnitTestingApp/Person.swift
+++ UnitTestingApp/UnitTestingApp/Person.swift
@@ -9,10 +9,23 @@ import Foundation
 
 class Person {
     let name: String
+    var surname: String?
     let phone: String
+    var image: Data?
+    private(set) var date: Date
     
-    init(name: String, phone: String) {
+    init(name: String, phone: String, image: Data? = nil) {
         self.name = name
         self.phone = phone
+        self.image = image
+        self.date = Date()
+    }
+    
+    init(name: String, surname: String?, phone: String, image: Data? = nil) {
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.image = image
+        self.date = Date()
     }
 }
--- UnitTestingApp/UnitTestingAppTests/PersonTests.swift
+++ UnitTestingApp/UnitTestingAppTests/PersonTests.swift
@@ -9,18 +9,63 @@ import XCTest
 @testable import UnitTestingApp
 
 final class PersonTests: XCTestCase {
+    
+    var person: Person?
+    var personWithFullName: Person?
+    var image: UIImage?
+    var imageData: Data?
 
     override func setUp() {
         super.setUp()
+        person = Person(name: "Foo", phone: "Bar")
+        personWithFullName = Person(name: "Foo", surname: "Bar", phone: "Baz")
+        image = UIImage(systemName: "person.crop.circle")
+        imageData = image?.pngData()
     }
 
     override func tearDown() {
+        person = nil
+        personWithFullName = nil
+        image = nil
+        imageData = nil
         
         super.tearDown()
     }
 
     func testInitPersonWithNameAndPhone() {
-        let person = Person(name: "Foo", phone: "Bar")
+        
+        XCTAssertNotNil(person)
+    }
+    
+    func testInitPersonWithFullNameAndPhone() {
+        XCTAssertNotNil(person)
+    }
+    
+    func testWhenGivenNameAndPhoneSetNameAndPhone() {
+        XCTAssertEqual(person?.name, "Foo")
+        XCTAssertEqual(person?.phone, "Bar")
+    }
+    
+    func testWhenGivenFullNameAndPhoneSetFullNameAndPhone() {
+        XCTAssertEqual(personWithFullName?.name, "Foo")
+        XCTAssertEqual(personWithFullName?.surname, "Bar")
+        XCTAssertEqual(personWithFullName?.phone, "Baz")
+    }
+    
+    func testInitPersonWithDate() {
+        XCTAssertNotNil(person?.date)
+    }
+    
+    func testInitPersonWithImage() {
+        let person = Person(name: "Foo", phone: "Bar", image: imageData)
+        XCTAssertNotNil(person.image)
+        
+    }
+    
+    func testInitPersonWithFullNameAndImage() {
+        let person = Person(name: "Foo", surname: "Bar", phone: "Baz", image: imageData)
+        XCTAssertNotNil(person.image)
+        
     }
 
 
