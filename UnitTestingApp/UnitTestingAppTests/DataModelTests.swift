35f8c818176dc73c63dedeaf2118407d6fdbc0e9
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-09 10:50:48 +0300

    Add methods to test

--- UnitTestingApp/UnitTestingAppTests/DataModelTests.swift
+++ UnitTestingApp/UnitTestingAppTests/DataModelTests.swift
@@ -1,31 +1,36 @@
 //
-//  UnitTestingAppTests.swift
-//  UnitTestingAppTests
+//  DataModelTests.swift
+//  DataModelTests
 //
 //  Created by Арсентий Халимовский on 08.05.2023.
 //
 
 import XCTest
+@testable import UnitTestingApp
 
 final class DataModelTests: XCTestCase {
+    
+    // sut mean SystemUnderTests
+    var sut: DataModel!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        // initialisation of objects which i wan't to test
+        // load our object
+        super.setUp()
+        sut = DataModel()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        // unload our object
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testExample() {
+        
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
